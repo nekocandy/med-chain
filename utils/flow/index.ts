15269c0a2ff76e2c40a29741a66ef8c8dd633686
Piyush <bhangalepiyush@gmail.com>
2024-04-20 21:25:45 +0530

    feat: add init file

---
+++ utils/flow/index.ts
@@ -0,0 +1,43 @@
+import { config } from '@onflow/fcl'
+import flowJSON from '../../flow.json'
+
+const runtimeConfig = useRuntimeConfig().public
+const network: 'mainnet' | 'testnet' | 'emulator' = (runtimeConfig.NETWORK as 'mainnet' | 'testnet' | 'emulator') || 'emulator'
+
+const { NC_ADDRESS } = runtimeConfig
+const all_address: Record<string, string> = {}
+
+const fclConfigInfo = {
+  mainnet: {
+    accessNode: 'https://rest-mainnet.onflow.org',
+    discoveryWallet: 'https://fcl-discovery.onflow.org/authn',
+    discoveryAuthInclude: [],
+  },
+  testnet: {
+    accessNode: 'https://rest-testnet.onflow.org',
+    discoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn',
+    discoveryAuthInclude: [],
+  },
+  emulator: {
+    accessNode: 'http://127.0.0.1:8888',
+    discoveryWallet: 'http://localhost:8701/fcl/authn',
+    discoveryAuthInclude: [],
+  },
+}
+
+Object.entries(flowJSON.contracts)
+  .filter(([_, value]) => typeof value === 'object')
+  .map(([key, contract]) => all_address[`0x${key}`] = `0x${(contract as any).aliases[network]}`)
+
+config({
+  'app.detail.title': 'Something',
+  'app.detail.icon': 'https://pycz.dev/favicon.svg',
+  'flow.network': network,
+  'accessNode.api': fclConfigInfo[network].accessNode,
+  'discovery.wallet': fclConfigInfo[network].discoveryWallet,
+  'discovery.authn.include': fclConfigInfo[network].discoveryAuthInclude,
+  '0xNC': NC_ADDRESS,
+  ...all_address,
+}).load({ flowJSON })
+
+export {}
