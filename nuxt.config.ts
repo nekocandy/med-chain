65ca3b9385ff9f59449a0d59b9fe6b8da0e166d0
Piyush <bhangalepiyush@gmail.com>
2024-04-21 18:13:55 +0530

    feat: update title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
-      title: 'opinionated nuxt',
+      title: 'Med Chain',
       meta: [
         {
           name: 'description',
